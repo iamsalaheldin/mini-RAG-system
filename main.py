a2f2a1ee5c3efd152cd2a672bca98bb2363c437f
Ahmed Mohamed Salah eldin Ahmed <asalah@rasan.co>
2026-03-01 10:37:29 +0200

    Create routes with base route & update README file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-app = FastAPI()
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes import base
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Welcome to FastAPI!"}
+app = FastAPI()
+app.include_router(base.base_router)
